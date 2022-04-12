deb86e87d1d963cab1a40e6dc1379c1ba4bb747c
Danilo Vieira <danilorocha722@gmail.com>
2022-04-12 11:44:46 -0300

    First Commit

---
+++ src/main/java/com/cerc/paymentsRegister/PaymentsRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.cerc.paymentsRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cerc/paymentsRegister/PaymentsRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cerc.paymentsRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

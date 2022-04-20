009dacfb17eb3c7ae7d1abf188aa6acb5de47309
DevSottie <j.vitorsilva.dev@gmail.com>
2022-04-19 21:33:34 -0300

    adicionado variaveis para a classe PositionUtils

--- src/main/java/com/cerc/paymentsRegister/Utils/PositionUtils.java
+++ src/main/java/com/cerc/paymentsRegister/Utils/PositionUtils.java
@@ -3,9 +3,11 @@ package com.cerc.paymentsRegister.Utils;
 import com.cerc.paymentsRegister.model.Position;
 
 public class PositionUtils {
+	private static final String positionName = "Test Positon";
+	private static final String departmentName = "Test Department";
 	
 	public static Position generatePosition() {
-		Position positionTest = new Position(null, "Test Position", 3400.0, "Test Department");
+		Position positionTest = new Position(null, positionName, 3400.0, departmentName);
 		return positionTest;
 	}
 

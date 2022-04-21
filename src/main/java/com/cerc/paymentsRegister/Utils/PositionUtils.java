e9e1e5b10c3e023a136b7737a64dcd3c284a4b78
DevSottie <j.vitorsilva.dev@gmail.com>
2022-04-21 10:03:37 -0300

    Classes utils comentadas para corrigir conflitos

--- src/main/java/com/cerc/paymentsRegister/Utils/PositionUtils.java
+++ src/main/java/com/cerc/paymentsRegister/Utils/PositionUtils.java
@@ -6,9 +6,9 @@ public class PositionUtils {
 	private static final String positionName = "Test Positon";
 	private static final String departmentName = "Test Department";
 	
-	public static Position generatePosition() {
+	/*public static Position generatePosition() {
 		Position positionTest = new Position(null, positionName, 3400.0, departmentName);
 		return positionTest;
-	}
+	}*/
 
 }

87b68cd130ed991860ba6bcf0d5cc0d1ed8eb0a6
monia <monialodo@hotmail.com>
2022-05-06 16:10:37 -0300

    Adcionado as annotations do Swagger

--- src/main/java/com/cerc/paymentsRegister/controller/PayrollController.java
+++ src/main/java/com/cerc/paymentsRegister/controller/PayrollController.java
@@ -1,7 +1,13 @@
 package com.cerc.paymentsRegister.controller;
 
 import com.cerc.paymentsRegister.dto.PayrollDTO;
+import com.cerc.paymentsRegister.model.Position;
 import com.cerc.paymentsRegister.service.PayrollService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,29 +20,56 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/payroll")
+@Tag(name = "Folha de Pagamento", description = "Endpoints para gerenciamento da folha de pagamento")
 public class PayrollController {
 
-	@Autowired
-	private PayrollService service;
-	
-	@GetMapping(value = "/show-all")
-	public ResponseEntity <List<PayrollDTO>> findAllPayrolls(){
-		List<PayrollDTO> payrolls = service.findAll();
-		return ResponseEntity.ok().body(payrolls);
-	}
-	
-	@GetMapping(value = "/{id}")
-	public ResponseEntity <PayrollDTO> findByIdEmployee(@PathVariable Long id, @RequestParam String date) throws ParseException{
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		PayrollDTO payroll = null;
-		Date dateConsulting = sdf.parse(date);
-		Month month = Month.of(dateConsulting.getMonth() + 1);
-		Integer numberOfRecords = service.payrollExist(id,month);
-		if(numberOfRecords != 0) {
-			payroll = service.findPayrollMonthByIdEmployee(id, date);
-		}else {
-			payroll = service.generatePayroll(id, date);
-		}
-		return ResponseEntity.ok().body(payroll);
-	}
+    @Autowired
+    private PayrollService service;
+
+    @GetMapping(value = "/show-all")
+    @Operation(
+            summary = "Lista todas as folhas de pagamento",
+            tags = {"Folha de Pagamento"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Position.class))
+                    ),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+            }
+    )
+    public ResponseEntity<List<PayrollDTO>> findAllPayrolls() {
+        List<PayrollDTO> payrolls = service.findAll();
+        return ResponseEntity.ok().body(payrolls);
+    }
+
+    @GetMapping(value = "/{id}")
+    @Operation(
+            summary = "Lista as folhas de pagamento por Id",
+            tags = {"Folha de Pagamento"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Position.class))
+                    ),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+            }
+    )
+    public ResponseEntity<PayrollDTO> findByIdEmployee(@PathVariable Long id, @RequestParam String date) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        PayrollDTO payroll = null;
+        Date dateConsulting = sdf.parse(date);
+        Month month = Month.of(dateConsulting.getMonth() + 1);
+        Integer numberOfRecords = service.payrollExist(id, month);
+        if (numberOfRecords != 0) {
+            payroll = service.findPayrollMonthByIdEmployee(id, date);
+        } else {
+            payroll = service.generatePayroll(id, date);
+        }
+        return ResponseEntity.ok().body(payroll);
+    }
 }
--- src/main/java/com/cerc/paymentsRegister/controller/PositionController.java
+++ src/main/java/com/cerc/paymentsRegister/controller/PositionController.java
@@ -3,6 +3,11 @@ package com.cerc.paymentsRegister.controller;
 import com.cerc.paymentsRegister.dto.PositionDTO;
 import com.cerc.paymentsRegister.model.Position;
 import com.cerc.paymentsRegister.service.PositionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,38 +16,105 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value="/position")
+@Tag(name = "Cargos", description = "Endpoints para gerenciamento de cargos")
 public class PositionController {
 	
 	@Autowired
 	private PositionService service;
 	
 	@GetMapping
+	@Operation(
+			summary = "Lista todos os cargos",
+			tags = { "Cargos" },
+			responses = {
+					@ApiResponse(
+							description = "Success",
+							responseCode = "200",
+							content = @Content(mediaType = "application/json", schema = @Schema(implementation = Position.class))
+					),
+					@ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+					@ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+			}
+	)
 	public ResponseEntity<List<PositionDTO>> findAllPosition(){
 		List<PositionDTO> positions = service.findAll();
 		return ResponseEntity.ok().body(positions);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Position> findById(@PathVariable Long id){
+
+	@Operation(
+			summary = "Lista cargos por Id",
+			tags = { "Cargos" },
+			responses = {
+					@ApiResponse(
+							description = "Success",
+							responseCode = "200",
+							content = @Content(mediaType = "application/json", schema = @Schema(implementation = Position.class))
+					),
+					@ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+					@ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+			}
+	)
+	public ResponseEntity<Position> findById (@PathVariable Long id){
 		Position position = service.findById(id);
 		return ResponseEntity.ok().body(position);
 	}
 	
 	@PostMapping
+	@Operation(
+			summary = "Cria cargos",
+			tags = { "Cargos" },
+			responses = {
+					@ApiResponse(
+							description = "Success",
+							responseCode = "200",
+							content = @Content(mediaType = "application/json", schema = @Schema(implementation = Position.class))
+					),
+					@ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+					@ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+			}
+	)
 	public ResponseEntity<Void> insertPosition(@RequestBody Position position){
 		service.insertPosition(position);
 		return ResponseEntity.ok().build();
 	}
 	
 	@DeleteMapping(value = "/{id}")
+	@Operation(
+			summary = "Deleta cargos por Id",
+			tags = { "Cargos" },
+			responses = {
+					@ApiResponse(
+							description = "Success",
+							responseCode = "200",
+							content = @Content(mediaType = "application/json", schema = @Schema(implementation = Position.class))
+					),
+					@ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+					@ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+			}
+	)
 	public ResponseEntity<String> deletePosition(@PathVariable Long id) throws Exception{
 		service.deletePosition(id);
 		return ResponseEntity.ok().body("Cargo Id: " + id + " Deletado com sucesso." );
 	}
 	
 	@PutMapping(value = "/{id}")
+	@Operation(
+			summary = "Atualiza cargos por Id",
+			tags = { "Cargos" },
+			responses = {
+					@ApiResponse(
+							description = "Success",
+							responseCode = "200",
+							content = @Content(mediaType = "application/json", schema = @Schema(implementation = Position.class))
+					),
+					@ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+					@ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+			}
+	)
 	public Position updatePosition(@RequestBody Position position, @PathVariable Long id){
 		return service.updatePosition(id, position);
 	}
-	
+
 }
